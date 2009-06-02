4e1535fc7bfce475d0f9c0093e69769115e428d0
Justin Tulloss <Justin.Tulloss@palm.com>
2009-06-02 00:21:42 -0700

    Test builds now, and mostly fails

---
+++ test.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2009 Justin Tulloss
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "astrcmp.h"
+#include <stdio.h>
+
+char *strings[] = {
+    "Hello",
+    "World",
+    "hi",
+    "Hell",
+    "OR",
+    "Damn Kids",
+    "Kids"
+};
+
+
+void compare_and_print(int index1, int index2) {
+    float res1 = LevenshteinDistance(strings[index1], sizeof(strings[index1]),
+                                     strings[index2], sizeof(strings[index2]));
+    printf("Similarity between \"%s\" and \"%s\" is %f\n", 
+        strings[index1], strings[index2], res1);
+}
+
+int main(int argc, char* argv[]) {
+    compare_and_print(0, 0);
+    compare_and_print(0, 1);
+    compare_and_print(0, 2);
+    compare_and_print(0, 3);
+    compare_and_print(0, 4);
+    compare_and_print(0, 5);
+    compare_and_print(5, 6);
+    compare_and_print(6, 5);
+}
+
